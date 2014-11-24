afbf35868d4a769d2e142910b26204219ee899a1
duoduo369 <duoduo3369@gmail.com>
2014-11-24 23:53:22 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Setup file for easy installation"""
+from os.path import join, dirname
+from setuptools import setup
+
+
+version = __import__('paginator_plus').__version__
+
+LONG_DESCRIPTION = """django paginator plus"""
+
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(join(dirname(__file__), 'README.me')).read()
+    except IOError:
+        return LONG_DESCRIPTION
+
+
+setup(name='django-paginator-plus',
+      version=version,
+      author='duoduo369',
+      author_email='duoduo3369@gmail.com',
+      description='Django paginator plus',
+      license='MIT',
+      keywords='django, paginator',
+      url='https://github.com/duoduo369/django-paginator-plus',
+      packages=['paginator_plus'],
+      long_description=long_description(),
+      install_requires=['Django>=1.4',],
+      classifiers=['Framework :: Django',
+                   'Development Status :: 1 - Beta',
+                   'Topic :: Internet',
+                   'License :: MIT License',
+                   'Intended Audience :: Developers',
+                   'Environment :: Web Environment',
+                   'Programming Language :: Python :: 2.7'],
+      zip_safe=False)
